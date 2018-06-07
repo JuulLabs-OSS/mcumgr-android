93b265b86f00f5db1d30acea71ff6cd7da6eeea9
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-06-07 16:36:50 +0200

    FsManager added

---
+++ mcumgr-android-lib/src/main/java/io/runtime/mcumgr/response/fs/McuMgrFsDownloadResponse.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017-2018 Runtime Inc.
+ * Copyright (c) Intellinium SAS, 2014-present
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.runtime.mcumgr.response.fs;
+
+import io.runtime.mcumgr.response.McuMgrResponse;
+
+public class McuMgrFsDownloadResponse extends McuMgrResponse {
+	public int off;
+	public int len;
+	public byte[] data;
+}
