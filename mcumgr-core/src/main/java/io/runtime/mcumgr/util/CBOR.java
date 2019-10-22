8d24520bdf9c8b758cdeb34875d03049ea649ab6
Brian Giori <briangiori@gmail.com>
2019-10-22 16:48:03 -0700

    do not fail on unknown properties

--- mcumgr-core/src/main/java/io/runtime/mcumgr/util/CBOR.java
+++ mcumgr-core/src/main/java/io/runtime/mcumgr/util/CBOR.java
@@ -6,54 +6,148 @@
 
 package io.runtime.mcumgr.util;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.MapType;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 
-import java.io.ByteArrayInputStream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unused")
 public class CBOR {
-    private final static CBORFactory sFactory = new CBORFactory();
 
-    public static byte[] toBytes(Object obj) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        return mapper.writeValueAsBytes(obj);
+    private final static ObjectMapper MAPPER = new ObjectMapper(new CBORFactory())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    //*********************************************************************************************
+    // Encode
+    //*********************************************************************************************
+
+    /*
+     * Bytes
+     */
+    public static byte[] toBytes(@Nullable Object obj) throws IOException {
+        return MAPPER.writeValueAsBytes(obj);
     }
 
-    public static <T> T toObject(byte[] data, Class<T> type) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        return mapper.readValue(data, type);
+    //*********************************************************************************************
+    // Decode
+    //*********************************************************************************************
+
+    /*
+     * String
+     */
+    public static String toString(@NotNull byte[] data) throws IOException {
+        return MAPPER.readTree(data).toString();
     }
 
-    public static String toString(byte[] data) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        return mapper.readTree(data).toString();
+    /*
+     * Object
+     */
+    public static <T> T toObject(@NotNull byte[] data, @NotNull Class<T> type) throws IOException {
+        return MAPPER.readValue(data, type);
     }
 
-    @SuppressWarnings("RedundantThrows")
-    public static <T> String toString(T obj) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        return mapper.valueToTree(obj).toString();
+    /*
+     * Map
+     */
+    public static <T> Map<String, T> toMap(@NotNull byte[] data, @NotNull Class<T> type) throws IOException {
+        MapType mapType = MAPPER.getTypeFactory().constructMapType(LinkedHashMap.class, String.class, type);
+        return MAPPER.readValue(data, mapType);
     }
 
-    public static Map<String, String> toStringMap(byte[] data) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        TypeReference<HashMap<String, String>> typeRef =
-                new TypeReference<HashMap<String, String>>() {};
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
-        return mapper.readValue(inputStream, typeRef);
+    /*
+     * List
+     */
+    public static <T> List<T> toList(@NotNull byte[] data, @NotNull Class<T> type) throws IOException {
+        CollectionType listType = MAPPER.getTypeFactory().constructCollectionType(ArrayList.class, type);
+        return MAPPER.readValue(data, listType);
     }
 
-    public static Map<String, Object> toObjectMap(byte[] data) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(sFactory);
-        TypeReference<HashMap<String, Object>> typeRef =
-                new TypeReference<HashMap<String, Object>>() {
-                };
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
-        return mapper.readValue(inputStream, typeRef);
+    //*********************************************************************************************
+    // Decode Value
+    //*********************************************************************************************
+
+    /*
+     * Object
+     */
+    public static <T> T getObject(@NotNull byte[] data, @NotNull String key, @NotNull Class<T> type) throws IOException {
+        return MAPPER.convertValue(MAPPER.readTree(data).get(key), type);
+    }
+
+    /*
+     * Integer
+     */
+    public static int getInt(@NotNull byte[] data, @NotNull String key) throws IOException {
+        return assertNonNull(MAPPER.readTree(data).get(key), data, key).asInt();
+    }
+    public static int getInt(@NotNull byte[] data, @NotNull String key, int defaultValue) throws IOException {
+        JsonNode node = MAPPER.readTree(data).get(key);
+        return node == null ? defaultValue : node.asInt(defaultValue);
+    }
+
+    /*
+     * Boolean
+     */
+    public static boolean getBoolean(@NotNull byte[] data, @NotNull String key) throws IOException {
+        return assertNonNull(MAPPER.readTree(data).get(key), data, key).asBoolean();
+    }
+    public static boolean getBoolean(@NotNull byte[] data, @NotNull String key, boolean defaultValue) throws IOException {
+        JsonNode node = MAPPER.readTree(data).get(key);
+        return node == null ? defaultValue : node.asBoolean(defaultValue);
+    }
+
+    /*
+     * Double
+     */
+    public static double getDouble(@NotNull byte[] data, @NotNull String key) throws IOException {
+        return assertNonNull(MAPPER.readTree(data).get(key), data, key).asDouble();
+    }
+    public static double getDouble(@NotNull byte[] data, @NotNull String key, double defaultValue) throws IOException {
+        JsonNode node = MAPPER.readTree(data).get(key);
+        return node == null ? defaultValue : node.asDouble(defaultValue);
+    }
+
+    /*
+     * Long
+     */
+    public static long getLong(@NotNull byte[] data, @NotNull String key) throws IOException {
+        return assertNonNull(MAPPER.readTree(data).get(key), data, key).asLong();
+    }
+    public static long getLong(@NotNull byte[] data, @NotNull String key, long defaultValue) throws IOException {
+        JsonNode node = MAPPER.readTree(data).get(key);
+        return node == null ? defaultValue : node.asLong(defaultValue);
+    }
+
+    /*
+     * String
+     */
+    public static String getString(@NotNull byte[] data, @NotNull String key) throws IOException {
+        return assertNonNull(MAPPER.readTree(data).get(key), data, key).asText();
+    }
+    public static String getString(@NotNull byte[] data, @NotNull String key, String defaultValue) throws IOException {
+        JsonNode node = MAPPER.readTree(data).get(key);
+        return node == null ? defaultValue : node.asText(defaultValue);
+    }
+
+
+    //*********************************************************************************************
+    // Utility Methods
+    //*********************************************************************************************
+
+    private static JsonNode assertNonNull(JsonNode node, byte[] data, String key) throws IOException {
+        if (node == null) {
+            throw new IOException("Property \"" + key + "\"not found in object: " + CBOR.toString(data));
+        }
+        return node;
     }
 }
