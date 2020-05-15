b89997b3515db09ec26ab2b8c166e1df79c3343a
philips77 <aleksander.nowakowski@nordicsemi.no>
2020-05-15 20:48:27 +0200

    Theme improvements

--- sample/src/main/java/io/runtime/mcumgr/sample/fragment/mcumgr/StatsFragment.java
+++ sample/src/main/java/io/runtime/mcumgr/sample/fragment/mcumgr/StatsFragment.java
@@ -110,7 +110,7 @@ public class StatsFragment extends Fragment implements Injectable {
             final SpannableString spannable = new SpannableString(error);
             TypedValue typedValue = new TypedValue();
             spannable.setSpan(new ForegroundColorSpan(
-                            ContextCompat.getColor(requireContext(), R.color.error)),
+                            ContextCompat.getColor(requireContext(), R.color.colorError)),
                     0, error.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
             spannable.setSpan(new StyleSpan(Typeface.BOLD),
                     0, error.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
